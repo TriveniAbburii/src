ec860e15c93ca528328f23dbdb9d2034aaa13f8b
TriveniAbburii <152672881+TriveniAbburii@users.noreply.github.com>
2024-01-29 16:22:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+import './App.css';
+import Mantra1 from './Component/Component';
+//import Mantra1 from './Mantra/Task6';
+//import Mantra1 from './Task/Task5';
+//import Mantra from './Task/Task1';
+//import Mantra1 from './Task/Task5';
+
+//mport Mantra1 from './Mantra/Task6';
+
+//import Mantra1 from './Task/Task5';
+//import Mantra1 from './Project/Task4';
+//import Mantra from './Task/Task1';
+//import Mantra1 from './Project/Task4';
+//mport Mantra from './Task/Task1';
+//import Mantra1 from './Task/Task2';
+// import Mantra from './Task/Task1';
+// import Mantra1 from './Task/Task2';
+// import Mantra1 from './Task/Task';
+// import Mantra from './Task/Task1';
+// import Task from './Task/Task';
+
+function App() {
+  return (
+   <>
+{/*    
+   <Mantra1/> */}
+  {/* <Mantra/> */}
+  {/* <Mantra/> */}
+  {/* <Mantra1/> */}
+  {/* 
+ <Mantra1/> */}
+
+
+<Mantra1/>
+
+
+   </>
+ );
+}
+
+
+
+export default App;
